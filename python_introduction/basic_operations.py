2d5c8299bf26d3cd62cca604848f730efb76cbbf
syntacticsugarboyy <izyspikes@gmail.com>
2025-11-09 02:43:27 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 5
+number2 = 3
+
+some = 'Addition'
+difference = 'Subtraction'
+product = 'Multiplication'
+
+print ('{} of {:d} and {:d} is {:d}'. format(some, number1, number2, (number1 + number2)))
+print ('{} of {:d} and {:d} is {:d}'. format(difference, number1, number2, (number1 - number2)))
+print ('{} of {:d} and {:d} is {:d}'. format(product, number1, number2, (number1 * number2)))
